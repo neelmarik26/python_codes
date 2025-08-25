e39746f9be8adfe20cf29329483f61d657ee2249
neel26 <neelmarik26@gmail.com>
2025-08-25 09:24:28 +0530

    fix

--- advance/jarvis.py
+++ advance/jarvis.py
@@ -3,8 +3,15 @@ import pyttsx3
 # initialize the engine
 engine = pyttsx3.init()
 
+# get current rate
+rate = engine.getProperty('rate')
+print("Current rate:", rate)
+
+# set new rate (higher = faster, lower = slower)
+engine.setProperty('rate', 150)  # default is usually around 200
+
 # say something
-engine.say("""hii chhanda marik kamon achen.""")
+engine.say("hello chhanda marik")
 
 # run and wait until speaking is finished
 engine.runAndWait()
